ff43ffefb18a306364c1477451556f66c9b58ad9
Andrew Murray <andrew@anamnesisgames.com>
2024-01-28 14:07:15 -0500

    Mesh loader can create grid primitives. (planes)

--- external/MeshLoader/include/meshloader/MeshLoader.h
+++ external/MeshLoader/include/meshloader/MeshLoader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cinttypes>
+
 #include "meshloader/MeshLoaderStd.h"
 #include "meshloader/Mesh.h"
 
@@ -9,6 +11,7 @@ namespace MeshLoader
     {
     public:
         ML_DLL static bool LoadCubePrimitive(Mesh& outMesh, const float width = 1.0f, const float height = 1.0f, const float depth = 1.0f);
+        ML_DLL static bool LoadGridPrimitive(float planeWidth, float planeDepth, uint32_t gridX, uint32_t gridY, Mesh& outMesh);
         ML_DLL static bool LoadModel(const void* buffer, const size_t bufferSize, Mesh& outMesh);
     };
 }
--- external/MeshLoader/src/MeshLoader.cpp
+++ external/MeshLoader/src/MeshLoader.cpp
@@ -201,6 +201,62 @@ bool Loader::LoadCubePrimitive(Mesh& outMesh, const float width /*= 1.0f*/, cons
     return true;
 }
 
+bool Loader::LoadGridPrimitive(float planeWidth, float planeDepth, uint32_t gridX, uint32_t gridY, Mesh& outMesh)
+{
+	const uint32_t vertexCount = gridX * gridY;
+	const uint32_t indexCount = (gridX - 1) * (gridY - 1) * 2 * 3;
+
+    PrepareMesh(outMesh, vertexCount, indexCount);
+
+    if (outMesh.buffer.get() == nullptr)
+    {
+        return false;
+    }
+
+	float halfWidth = 0.5f * planeWidth;
+	float halfDepth = 0.5f * planeDepth;
+
+	float dx = planeWidth / (gridY - 1);
+	float dz = planeDepth / (gridX - 1);
+
+	float du = 1.0f / (gridY - 1);
+	float dv = 1.0f / (gridX - 1);
+
+	for (uint32_t i = 0; i < gridX; ++i)
+	{
+		float z = halfDepth - i*dz;
+		for (uint32_t j = 0; j < gridY; ++j)
+		{
+			float x = -halfWidth + j*dx;
+
+            outMesh.GetVertex()[i*gridY+j].position = { x, 0.0f, z };
+            outMesh.GetVertex()[i*gridY+j].normal = { 0.0f, 1.0f, 0.0f };
+            outMesh.GetVertex()[i*gridY+j].tangent = { 1.0f, 0.0f, 0.0f };
+            outMesh.GetVertex()[i*gridY+j].texture = { j * du, i * dv };
+		}
+	}
+
+	// Iterate over each quad and compute indices.
+	uint32_t k = 0;
+	for(uint32_t i = 0; i < gridX - 1; ++i)
+	{
+		for(uint32_t j = 0; j < gridY - 1; ++j)
+		{
+			outMesh.GetIndex()[k]  = i*gridY+j;
+			outMesh.GetIndex()[k+1] = i*gridY+j+1;
+			outMesh.GetIndex()[k+2] = (i+1)*gridY+j;
+
+			outMesh.GetIndex()[k+3] = (i+1)*gridY+j;
+			outMesh.GetIndex()[k+4] = i*gridY+j+1;
+			outMesh.GetIndex()[k+5] = (i+1)*gridY+j+1;
+
+			k += 6; // next quad
+		}
+	}
+
+    return true;
+}
+
 bool Loader::LoadModel(const void* buffer, const size_t bufferSize, Mesh& outMesh)
 {
 #ifdef ML_ENABLE_ASSIMP
